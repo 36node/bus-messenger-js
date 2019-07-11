b3fbfff557151a3916972bfc4e3094818a2631f8
zzswang <zzswang@gmail.com>
2019-07-11 21:37:36 +0800

    fix: tbox data format

--- src/station.js
+++ src/station.js
@@ -44,18 +44,52 @@ export class Station extends Transform {
       next(res);
     }
   }
-
-  _flush(cb) {
-    cb();
-  }
 }
 
 /**
  * TBox 数据中转站
  */
 export class TBoxStation extends Station {
+  /**
+   * 消息中转站构造函数
+   *
+   * @param {IHandler} handler
+   */
+  constructor(handler = noop) {
+    super({ objectMode: true });
+    this._handler = handler;
+  }
+
   get name() {
-    return "Tbox";
+    return "TBox Station";
+  }
+
+  /**
+   * 自定义处理函数
+   *
+   * @param {object} data 数据统一处理成对象
+   * @param {string} encoding 编码
+   * @param {function} next 回调 next(err, data) 第二个参数是需要传递给下一个 mailbox 的数据
+   */
+  _transform(data, encoding, next) {
+    // 将数据做一次转换
+    const { platform, time, session, seq, request } = data;
+
+    // TboxStation 只关心请求数据
+    if (!request || typeof request !== "object") {
+      return next();
+    }
+
+    const res = this._handler(
+      { platform, time, session, seq, ...request },
+      this.push.bind(this)
+    );
+
+    if (res instanceof Promise) {
+      res.then(() => next()).catch(err => next(err));
+    } else {
+      next();
+    }
   }
 }
 
